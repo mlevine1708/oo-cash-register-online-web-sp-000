bf6a09c43173957a1edf8e2cfd2537ca111915a6
Micah Levine <micah.levine@outlook.com>
2019-09-21 00:19:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ def initialize(discount = 0)
   @items = []
 end
 
-def add_item(item, price, quantity = 1)
+def add_item(title, amount, quantity = 1)
   self.total += amount * quantity
   quantity.times do
     items << title
